d5b8c8b68e0d73336303a3060925c8a6c5422803
Ben Cohen-Chen <ben@bencohen.dev>
2023-03-05 11:25:05 -0500

    barebones link to bandcamp

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Eschatology Records Is Undergoing A Full Rebuild</h1>
+<p><a href="https://eschatologyrecords.bandcamp.com/">Find us on Bandcamp for now</a></p>
