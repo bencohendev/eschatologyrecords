660a152e512574ba0b6eb4b452dcda1bbab76beb
Ben Cohen-Chen <ben@bencohen.dev>
2023-03-05 11:29:05 -0500

    added contact email

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
 <h1>Eschatology Records Is Undergoing A Full Rebuild</h1>
 <p><a href="https://eschatologyrecords.bandcamp.com/">Find us on Bandcamp for now</a></p>
+<div>
+	<h3>Contact</h3>
+	<a href="mailto:ben@eschatologyrecords.com">ben at eschatologyrecords.com</a>
+</div>
