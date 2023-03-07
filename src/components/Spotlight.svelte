54f22d8c0458e935abdc2808ec1db9d6e0b46157
Ben Cohen-Chen <ben@bencohen.dev>
2023-03-06 22:45:49 -0500

    annoying logo animation

---
+++ src/components/Spotlight.svelte
@@ -0,0 +1,37 @@
+<script>
+	let el;
+
+	let right = '50%';
+	let top = '50%';
+	let max = 80;
+	let min = 1;
+
+	setInterval(() => {
+		right = `${Math.floor(Math.random() * (max - min + 1) + min)}%`;
+		top = `${Math.floor(Math.random() * (max - min + 1) + min)}%`;
+	}, 1000);
+</script>
+
+<div
+	style:right
+	style:top
+	bind:this={el}
+	class="absolute w-1/4 h-0 transition-all ease-in duration-1000  bg-transparent move"
+>
+	<!-- <div class="spotlight w-full h-full" /> -->
+	<div class="flex items-stretch spotlight">
+		<div class="spotlight w-full" />
+		<img class="bg-transparent" src="./logo_midgreen_spotlight.png" alt="spotlight" />
+	</div>
+</div>
+
+<style>
+	.spotlight {
+		border-radius: 10000px;
+		-webkit-box-shadow: 68px 93px 133px -18px rgba(0, 0, 0, 0.75);
+		-moz-box-shadow: 68px 93px 133px -18px rgba(0, 0, 0, 0.75);
+		box-shadow: 68px 93px 133px -18px rgba(0, 0, 0, 0.75);
+	}
+	.move {
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,28 @@
-<h1>Eschatology Records Is Undergoing A Full Rebuild</h1>
-<p><a href="https://eschatologyrecords.bandcamp.com/">Find us on Bandcamp for now</a></p>
-<div>
-	<h3>Contact</h3>
-	<a href="mailto:ben@eschatologyrecords.com">ben at eschatologyrecords.com</a>
+<script>
+	import Spotlight from '../components/Spotlight.svelte';
+</script>
+
+<div class="grid h-screen place-items-center">
+	<div>
+		<div class="flex items-center justify-center">
+			<h1 class="text-2xl my-8">Eschatology Records Is Undergoing A Full Rebuild</h1>
+		</div>
+		<div class="flex justify-center">
+			<div>
+				<div>
+					<div>Until then...</div>
+					<a class="text-blue-500 text-xl" href="https://eschatologyrecords.bandcamp.com/"
+						>Find us on Bandcamp</a
+					>
+				</div>
+				<div class="my-4">
+					<h3 class="text-lg">Contact</h3>
+					<a class="text-blue-500" href="mailto:ben@eschatologyrecords.com"
+						>ben at eschatologyrecords.com</a
+					>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
+<Spotlight />
